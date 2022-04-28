a5740e9b06f68c008c1e4a898cc14edb41afe53f
Slickerish <landon@live.ca>
2022-04-27 21:20:16 -0600

    Rewrite JS and add scoreboard to HTML

--- script.js
+++ script.js
@@ -1,48 +1,29 @@
-let newGame = document.querySelector('#newgame');
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const content = document.querySelector("#winner");
+const scoreboard = document.querySelector('#scoreboard');
+const gameOver = document.querySelector('#gameover');
 
-let leftCol = document.querySelector('#left');
-let middleCol = document.querySelector('#center');
-let rightCol = document.querySelector('#right');
+let playerGuess = "";
+let score = [0, 0];
 
-let rock = document.createElement('button');
-let paper = document.createElement('button');
-let scissors = document.createElement('button')
+rock.addEventListener("click", () => {
+  playerGuess = "rock";
+  playGame(playerGuess);
+});
 
-let winner = "";
+paper.addEventListener("click", () => {
+  playerGuess = "paper";
+  playGame(playerGuess);
+});
 
-rock.classList.add = 'btn btn-outline-primary';
-paper.classList.add = 'btn btn-outline-primary';
-scissors.className.add = 'btn btn-outline-primary';
+scissors.addEventListener("click", () => {
+  playerGuess = "scissors";
+  playGame(playerGuess);
+});
 
-rock.setAttribute('id', 'rock');
-paper.setAttribute('id', 'paper');
-scissors.setAttribute('id', 'scissors');
-
-rock.textContent = "Rock";
-paper.textContent = "Paper";
-scissors.textContent = "Scissors";
-
-newGame.addEventListener('click', newGameFunction);
-
-let win = document.createElement('h3');
-
-let winbox = document.querySelector('#winner')
-
-function newGameFunction() {
-  leftCol.appendChild(rock);
-  middleCol.appendChild(paper);
-  rightCol.appendChild(scissors);
-  addClassesFunction();
-  playRound(playerPlay(), computerPlay());
-}
-
-function addClassesFunction() {
-  rock.className = "btn btn-outline-danger my-4";
-  paper.className = "btn btn-outline-danger my-4";
-  scissors.className = "btn btn-outline-danger my-4";
-}
-
-// Generate computers guess
+// Generate computer guess
 function computerPlay() {
   switch (Math.floor(Math.random() * 3)) {
     case 0:
@@ -55,64 +36,57 @@ function computerPlay() {
       computerGuess = "scissors";
       break;
   }
-  return computerGuess;
+  return computerGuess
 }
 
-// Player's selection
-function playerPlay() {
-  rock.addEventListener('click', () => {
-    playerGuess = "rock";
-  })
-  paper.addEventListener('click', () => {
-    playerGuess = "paper";
-  })
-  scissors.addEventListener('click', () => {
-    playerGuess = "scissors";
-  })
-  return playerGuess;
-}
-
-// Play a round
-function playRound(player, computer) {
+// Play a game
+function playGame (player) {
+  let computer = computerPlay();
   if (player == computer) {
-    console.log("It's a draw!");
+    content.textContent = "It's a draw!";
   } else if (player == "rock") {
     if (computer == "paper") {
-      console.log("Paper covers rock - Computer wins!");
-      winner = "computer";
+      content.textContent = "Paper covers rock - Computer wins!";
+      ++score[1];
     } else {
-      console.log("Rock breaks scissors - Player wins!");
-      winner = "player";
+      content.textContent = "Rock breaks scissors - Player wins!";
+      ++score[0];
     }
   } else if (player == "paper") {
     if (computer == "scissors") {
-      console.log("Scissors cut paper - Computer wins!");
-      winner = "computer";
+      content.textContent = "Scissors cut paper - Computer wins!";
+      ++score[1];
     } else {
-      console.log("Paper covers rock - Player wins!");
-      winner = "player";
+      content.textContent = "Paper covers rock - Player wins!";
+      ++score[0];
     }
   } else if (player == "scissors") {
     if (computer == "rock") {
-      console.log("Rock breaks scissors - Computer wins!");
-      winner = "computer";
+      content.textContent = "Rock breaks scissors - Computer wins!";
+      ++score[1];
     } else {
-      console.log("Scissors cut paper - Player wins!");
-      winner = "player";
+      content.textContent = "Scissors cut paper - Player wins!";
+      ++score[0];
     }
   } else {
-    console.log("I did not understand that!");
+    content.textContent = "I did not understand that!";
   }
-  if (winner == "computer") {
-    win.textContent = "You Lose :(";
-  } else if (winner = "player") {
-    win.textContent = "You Win! :D";
-  } else {
-    win.textContent = "It's a draw";
+  scoreboard.textContent = "Player: " + score[0] + "  ---  Computer: " + score[1];
+  if (score[0] == 5) {
+    rock.style.display = "none";
+    paper.style.display = "none";
+    scissors.style.display = "none";
+    gameOver.textContent = "Player got to 5 first! Player wins!";
+  } else if (score[1] == 5) {
+    rock.style.display = "none";
+    paper.style.display = "none";
+    scissors.style.display = "none";
+    gameOver.textContent = "Computer got to 5 first! Computer wins!";
   }
-  winbox.appendChild(win);
 }
-   // Generate computers guess
+
+
+    // Generate computers guess
 //    function computerPlay() {
 //      switch (Math.floor(Math.random() * 3)) {
 //        case 0:
@@ -173,7 +147,7 @@ function playRound(player, computer) {
 //    function game() {
 //      for (let i = 0; i < 5; i++) {
 //        playRound(playerPlay(), computerPlay());
-//        
+//
 //      }
 //      console.log(`Final Score - Player: ${score[0]} Computer: ${score[1]}`);
 //      if (score[0] > score [1]) {
